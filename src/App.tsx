1fdb0976e1f06a346489aec2d112dafe9676596e
Javier <javierdepablos04@gmail.com>
2026-07-01 13:20:45 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./views/Home"
+import LaCadena from "./views/LaCadena"
 
 function App() {
 
+
+
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= { <Home />}></Route>
+        <Route path="/la-cadena" element= { <LaCadena />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/views/LaCadena.tsx
@@ -0,0 +1,9 @@
+
+
+const LaCadena = () => {
+    return (
+        <></>
+    )
+}
+
+export default LaCadena;
