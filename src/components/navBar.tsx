3797d5201c0273e7d794f04339c3d5f654653279
Javier <javierdepablos04@gmail.com>
2026-07-03 00:03:42 +0200

    gsap animations

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,7 +1,21 @@
 import "./navBar.css"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
 
 
 const NavBar = () => {
+
+    useGSAP(() => {
+        const nav = document.querySelector("nav");
+
+        gsap.from(nav, {
+        y : -70,  
+        ease : "power1.out",
+        duration : 1,
+        delay : 2
+        })
+    })
+
     return (
         <nav>
             <div id="links-container">
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,58 @@
 import "./Home.css"
-import logoImg from "../assets/logo.svg"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import SplitText from "gsap/src/SplitText";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
 
 const Home = () => {
+
+
+    useGSAP(() => {
+
+        const tl = gsap.timeline();
+        const split = SplitText.create("#animated-text", {type:"chars"});
+        const h2 = document.getElementById("h2-animation");
+        const p = document.getElementById("p-animation");
+
+        tl.from(split.chars, {
+            fontSize : 70,
+            x : 0,
+            opacity : 0,
+            duration : 0.6,
+            ease : "power3.out",
+            stagger : 0.15,})
+        tl.from(h2, {
+            y : 40,
+            opacity : 0,
+            duration : 0.5,
+            ease : "power3.out",
+        })
+        tl.from(p, {
+            y : 40,
+            opacity : 0,
+            duration : 0.5,
+            ease : "power3.out",
+        }, "-=0.5")
+
+
+    });
+
+        
     return (
         <>
             <div className="background b0">      
                 <div id="content">
                     <div id="logo-container">
-                        <img src= {logoImg} alt="Logo img"/>
+                        <span id="animated-text" className="span-container text">
+                            ATALAYA DE PERALTA
+                        </span>
                     </div>  
                     <div className="text-container">
-                        <h2>Haz realidad tu sueño</h2>
+                        <h2 id="h2-animation" className="text">Haz realidad tu sueño</h2>
 
-                        <p>
+                        <p id="p-animation" className="text">
                             En Atalaya de Peralta nos encargamos
                             de proporcionarte la vivienda que mas
                             se ajuste a tus necesidades.
